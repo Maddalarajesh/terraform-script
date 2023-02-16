10a6976e4a128335f5eb3010ce6dba992e4796e1
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2023-02-16 09:56:17 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 sudo yum -y install git
-git clone https://github.com/Maddalarajesh/terraform.git
+git clone https://github.com/Maddalarajesh/terraform-script.git
 cd terraform
 terraform init
 terraform validate
